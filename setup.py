a15004f6512d0480298ef285bd9eda73f9387cfe
Filipe Pina <fopina@skmobi.com>
2015-07-16 19:02:34 +0100

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='tgbotplug',
-    version='0.1.3',
+    version='0.1.4',
     packages=['tgbot'],
     include_package_data=True,
     license='MIT License',
