933d53916433d4b1ff2b271118660a80aa654197
Filipe Pina <fopina@gmail.com>
2015-07-31 15:22:56 +0100

    webserver exposing wsgi application, close #8

--- tgbot/webserver.py
+++ tgbot/webserver.py
@@ -10,14 +10,20 @@ def ping():
 
 
 @route('/update/<token>', method='POST')
-def test(token):
+def update(token):
     if token != tg_bot._token:
         abort(404, 'Not found: \'/update/%s\'' % token)
     tg_bot.process_update(Update.from_dict(request.json))
     return None
 
 
-def run_server(bot, **kwargs):
+def wsgi_app(bot):
     global tg_bot
     tg_bot = bot
+    from bottle import default_app
+    return default_app()
+
+
+def run_server(bot, **kwargs):
+    wsgi_app(bot)
     run(**kwargs)
